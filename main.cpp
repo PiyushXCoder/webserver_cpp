85d95f684199d707d9aa12da891180cba9d547bd
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-03-12 18:53:01 +0530

    Fixed issue about port becoming TIME-WAIT after exit in testing (Wasn't fixed)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-
+#include <cerrno>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -9,23 +9,25 @@
 #include <sys/socket.h>
 
 int main() {
-    int socket_fd = socket(AF_INET, SOCK_STREAM | SOCK_CLOEXEC, 0 /* IP */);
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0 /* IP */);
 
     int opts = 1;
-    setsockopt(socket_fd, IPPROTO_TCP, SO_REUSEADDR | SO_REUSEPORT, &opts,
-               sizeof(opts));
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opts,
+                   sizeof(opts)) < 0) {
+        std::cout << "Failed to set opts: " << strerror(errno) << std::endl;
+    }
 
     sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(8000);
     if (bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr))) {
-        std::cout << "Failed to bind" << std::endl;
+        std::cout << "Failed to bind: " << strerror(errno) << std::endl;
         exit(1);
     }
 
     if (listen(socket_fd, 3)) {
-        std::cout << "Failed to listen" << std::endl;
+        std::cout << "Failed to listen: " << strerror(errno) << std::endl;
         exit(1);
     }
 
@@ -34,7 +36,7 @@ int main() {
     int socket_c_fd;
     if ((socket_c_fd =
              accept(socket_fd, (struct sockaddr *)&addr, &addr_c_len)) < 0) {
-        std::cout << "Failed to accept" << std::endl;
+        std::cout << "Failed to accept: " << strerror(errno) << std::endl;
         exit(1);
     }
 
@@ -44,7 +46,6 @@ int main() {
     char buf[buf_len];
     // read(socket_c_fd, buf, buf_len);
     recv(socket_c_fd, buf, buf_len, MSG_DONTWAIT);
-    //
     std::cout << buf << std::endl;
 
     char msg[] = "HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nHello World!";
@@ -57,8 +58,10 @@ int main() {
 
     std::cout << "closing" << std::endl;
 
-    std::cout << "close c: " << close(socket_c_fd) << std::endl;
-    std::cout << "close c: " << close(socket_fd) << std::endl;
+    if (close(socket_fd) < 0) {
+        std::cout << "Failed to close: " << strerror(errno) << std::endl;
+        exit(1);
+    }
 
     return 0;
 }
